7368967efe6f3c472fbcd9fd6dd32ba939c6c408
Andrew Gendy <andrewgendyawad@gmail.com>
2025-04-14 14:59:43 -0400

    partner 2 functions

--- calculator.py
+++ calculator.py
@@ -25,5 +25,28 @@ def logarithm(a, b):
         raise ValueError("Logarithm argument must be positive")
     return math.log(b, a)
 
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm.")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 def exponent(a, b):
     return a ** b
